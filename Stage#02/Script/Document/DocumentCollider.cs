10a1167c801a69a4f8e850288fdb2eb89ae7d552
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-08-26 21:42:48 +0900

    Add files via upload

--- Stage#02/Script/Document/DocumentCollider.cs
+++ Stage#02/Script/Document/DocumentCollider.cs
@@ -7,18 +7,30 @@ public class DocumentCollider : MonoBehaviour
     private GameObject       player;
     [SerializeField]
     private PlayerController playerController;
-    
+    [SerializeField]
+    private GameObject       blueAnimation;
+    [SerializeField]
+    private GameObject       redAnimation;
+
+    private TimeLimit        timeLimit;
+
+    private void Awake() {
+        timeLimit = player.GetComponent<TimeLimit>();
+    }
+
     private IEnumerator OnTriggerExit2D(Collider2D collision) {
         // Correct
-        if (((collision.gameObject.CompareTag("DocumentBlue")) && (playerController.isRed == false))
-         || ((collision.gameObject.CompareTag("DocumentRed" )) && (playerController.isRed == true ))) {
-            player.GetComponent<TimeLimit>().IncreaseTime();
-            Debug.Log("Correct");
+        if      (collision.gameObject.CompareTag("DocumentBlue") && playerController.isRed == false) {
+            timeLimit.IncreaseTime();
+            Instantiate(blueAnimation,  new Vector3(0.4f, 0.3f, 0), Quaternion.identity);
+        }
+        else if (collision.gameObject.CompareTag("DocumentRed" ) && playerController.isRed == true ) {
+            timeLimit.IncreaseTime();
+            Instantiate(redAnimation,   new Vector3(-0.5488f, 0.2f, 0), Quaternion.identity);
         }
         // Incorrect
         else {
-            player.GetComponent<TimeLimit>().ReduceTime();
-            Debug.Log("Incorrect");
+            timeLimit.ReduceTime();
         }
         
         Destroy(collision.gameObject);
