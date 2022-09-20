457fc6711d3aa95d5eeb0fcd4a40719e164bedd5
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-09-20 11:28:59 +0900

    Add files via upload

--- Stage#01/Script/LaunchMissile.cs
+++ Stage#01/Script/LaunchMissile.cs
@@ -1,10 +1,88 @@
+using System.Collections;
 using UnityEngine;
 
-public class LaunchMissile : MonoBehaviour {
+public class LaunchMissile : MonoBehaviour
+{
     [SerializeField]
-    private GameObject Missile;
+    private GameObject Sky;
+    [SerializeField]
+    private GameObject Desk;
+    [SerializeField]
+    private GameObject Hand;
+    [SerializeField]
+    private GameObject Button;
+    [SerializeField]
+    private GameObject Missile_1;
+    [SerializeField]
+    private GameObject Missile_2;
+    [SerializeField]
+    private GameObject Missile_3;
 
-    public void Luanch() {
-        Instantiate(Missile, new Vector3(-4.06f, 0.23f, 0.0f), Quaternion.identity);
+    private SpriteRenderer deskSpriteRenderer;
+    private SpriteRenderer buttonSpriteRenderer;
+    private float          rgb;
+    private bool           first = true;
+
+    public void Awake() {
+        deskSpriteRenderer = Desk.GetComponent<SpriteRenderer>();
+        buttonSpriteRenderer = Button.GetComponent<SpriteRenderer>();
+    }
+
+    public void Launch() {
+        if (first) {
+            StartCoroutine("PushButton");
+            StartCoroutine("BackgroundDim");
+
+            var Object = Instantiate(Missile_1, new Vector3(0f, -5.0f, 0f), Quaternion.identity);
+                         Instantiate(Missile_2, new Vector3(0f, -5.0f, 0f), Quaternion.identity);
+                         Instantiate(Missile_3, new Vector3(0f, -5.0f, 0f), Quaternion.identity);
+
+            GameManager.Instance.LoadStage(
+                () => { return Object != null; },
+                GameManager.Instance.CutScene2,
+                false ,2.0f
+            );
+
+            first = false;
+        }
+    }
+
+    private IEnumerator PushButton() {
+        Hand.SetActive(true);
+        Button.SetActive(false);
+        yield return new WaitForSeconds(0.3f);
+        Hand.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        Button.SetActive(true);
+    }
+
+    private IEnumerator BackgroundDim() {
+        Color deskColor = deskSpriteRenderer.color;
+        Color buttonColor = buttonSpriteRenderer.color;
+        rgb = 255f;
+        while (rgb > 60f) {
+            yield return new WaitForSeconds(0.1f);
+            deskColor   = new Color (rgb/255f, rgb/255f, rgb/255f, 1f);
+            buttonColor = new Color (rgb/255f, rgb/255f, rgb/255f, 1f);
+            deskSpriteRenderer.color   = deskColor;
+            buttonSpriteRenderer.color = buttonColor;
+            rgb -= 5f;
+        }
+
+        StartCoroutine("BackgroundBrightening");
+    }
+
+    private IEnumerator BackgroundBrightening() {
+        Color deskColor = deskSpriteRenderer.color;
+        Color buttonColor = buttonSpriteRenderer.color;
+
+        while (rgb < 255f) {
+            yield return new WaitForSeconds(0.1f);
+            deskColor   = new Color (rgb/255f, rgb/255f, rgb/255f, 1f);
+            buttonColor = new Color (rgb/255f, rgb/255f, rgb/255f, 1f);
+            deskSpriteRenderer.color   = deskColor;
+            buttonSpriteRenderer.color = buttonColor;
+            rgb += 12f;
+        }
     }
 }
--- Stage#01/Script/Missile.cs
+++ Stage#01/Script/Missile.cs
@@ -1,7 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class Missile : MonoBehaviour {
+    private SpriteRenderer spriteRenderer;
+    private float speed;
+    [SerializeField]
+    private bool           isFront = false;
+    [SerializeField]
+    private bool           isFirst = false;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (isFront == true) {
+            StartCoroutine("TwinkleLoop");
+        }
+    }
+
     public void Update () {
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(4.6f, 5.45f, 0f), 0.1f);
+        float speed = 2.3f * Time.deltaTime;
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(0f, 7.0f, 0f), speed);
+    }
+
+    private IEnumerator TwinkleLoop() {
+        while (true) {
+            if (isFirst) {
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            Color color = spriteRenderer.color;
+            if (color.a == 255f) {
+                color.a = 0f;
+            }
+            else {
+                color.a = 255;
+            }
+            spriteRenderer.color = color;
+        yield return null;
+        }
     }
 }
